8fdc9cc1d6d970e1ebe4bb632dfa0f31f58eef52
Ijon Tichy <gztichy@lavabit.com>
2011-07-17 15:18:36 -0500

    Added some weapons, and am restructuring things in the new PK3

---
+++ pk3-old/acs/jkemod.c
@@ -0,0 +1,30 @@
+#library "jkemod"
+#include "zcommon.acs"
+
+script 900 (int trigger)
+{
+    SetPlayerproperty(0, trigger, 0);
+}
+
+Script 901 (int line)
+{
+    switch (line)
+    {
+      case 0:
+        Print(s:"Missile launcher mode");
+        break;
+
+      case 1:
+        Print(s:"Shotgun ice launcher mode");
+        break;
+
+      case 2:
+        Print(s:"Fire spread launcher mode");
+        break;
+    }
+}
+
+Script 902 (int speed)
+{
+    SetActorProperty(0, APROP_Speed, speed);
+}
