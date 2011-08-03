3039a7599d21b201317b01449345948ff13e2ca8
Ijon Tichy <gztichy@lavabit.com>
2011-08-03 10:22:18 -0500

    + Added SpitFirer (formerly SMGSorc) * Made the M249 and M16 more accurate - I mean come on, their spread shouldn't be that of a shotgun * +IGNORESKILL'd SuperShotNukeCounter * Added a check to FModMinigun's Shoot state to check for lack of ammo - originally, the weapon would lower when you let go without ammo and semi-permamently slow you ? That damned "no MainFired/AltFired" bug is back - I really have no clue what's causing it ? DukePistol put in unadapted/ folder - I'll put it in once I get the proper sprites for it

--- pk3/acs/mousecheck.c
+++ pk3/acs/mousecheck.c
@@ -3,6 +3,7 @@
 
 
 int dead[32];
+int inGame[32];
 
 script 759 OPEN
 {
@@ -17,21 +18,18 @@ script 760 (void)   /* generic attack/alt attack handler */
 {
     int pln = PlayerNumber();
     int buttons;
-    while (!dead[pln])
+    while (!dead[pln] && inGame[pln])
     {
         buttons = GetPlayerInput(-1, MODINPUT_BUTTONS);
         if (buttons & BT_ATTACK)
         { GiveInventory("MainFired", 1); }
-
         else
         { TakeInventory("MainFired", 16); }
 
         if (buttons & BT_ALTATTACK)
         { GiveInventory("AltFired", 1); }
-
         else
         { TakeInventory("AltFired", 16); }
-
 
         Delay(1);
     }
@@ -40,6 +38,7 @@ script 760 (void)   /* generic attack/alt attack handler */
 script 761 ENTER
 {
     TakeInventory("PlayerDead", 1); // y'know, assuming you have it
+    inGame[PlayerNumber()] = 1;
     dead[PlayerNumber()] = 0;
     ACS_ExecuteAlways(760, 0, 0,0,0);
 }
@@ -47,15 +46,20 @@ script 761 ENTER
 script 762 DEATH
 {
     GiveInventory("PlayerDead", 1);
+    inGame[PlayerNumber()] = 1;
     dead[PlayerNumber()] = 1;
 }
 
 script 763 (int pln) DISCONNECT
 {
     dead[PlayerNumber()] = 0;
+    inGame[PlayerNumber()] = 0;
 }
 
 script 764 RESPAWN
 {
-    ACS_ExecuteAlways(761, 0, 0,0,0);
+    TakeInventory("PlayerDead", 1);
+    inGame[PlayerNumber()] = 1;
+    dead[PlayerNumber()] = 0;
+    ACS_ExecuteAlways(760, 0, 0,0,0);
 }
