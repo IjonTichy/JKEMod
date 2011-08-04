990a06a24f10a7ab80939dc4d332df4186eaca5c
Ijon Tichy <gztichy@lavabit.com>
2011-08-04 08:47:25 -0500

    + Added JRocketLauncher (formerly JModRLauncher) + Added spaceCaptials.py - this is probably going to be used for spacing names once the adapting phase is done (it shouldn't change the BDW) + Added a weapon list * Fixed the Spitfirer not having a Spawn sprite or LANGUAGE entries * Added jkemod*.zip and jkemod*.pk3 to the .gitignore - I don't need to put release on here, you know

---
+++ spaceCapitals.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+# Desired behaviour:
+#  potato = potato
+#  Potato = Potato
+#  potAto = pot Ato
+#  poTAto = po TAto
+#  poTaTo = po Ta To
+#  POT700 = POT 700
+
+import string
+
+class NextLetter(Exception): pass
+class Done(Exception): pass
+
+UPPERCASE   = string.ascii_uppercase   # for shorter comparisons
+LOWERCASE   = string.ascii_lowercase
+LETTERS     = UPPERCASE + LOWERCASE
+SPACES      = string.whitespace
+NUMBERS     = "0123456789"
+
+
+inp = ""
+
+try:
+    while inp != "quit":
+
+        try:
+            inp = input()
+
+        except EOFError:
+            raise Done
+
+        else:
+            if inp == "quit":
+                raise Done
+
+        newString = []
+        newInput = inp.strip()
+        newLen = len(newInput)
+
+        for n, i in enumerate(newInput):
+
+            try:
+                if n == 0:
+                    newString.append(i)
+                    raise NextLetter
+
+
+                if i in UPPERCASE:
+                    if newInput[n - 1] in LOWERCASE:
+                        newString.extend(" ")
+
+                    if newInput[n - 1] in UPPERCASE and (n + 1 != newLen):
+                        if newInput[n + 1] in LOWERCASE:
+                            newString.extend(" ")
+
+
+                if i in NUMBERS:
+                    if newInput[n - 1] not in LETTERS:
+                        newString.extend(i)
+                    else:
+                        newString.extend(" " + i)
+
+                    raise NextLetter
+
+                newString.extend(i)
+
+
+            except NextLetter:
+                pass
+
+
+
+
+        out = "".join(newString)
+
+        if " " not in out:
+            out = " {0} ".format(out)
+
+        print(out)
+except Done:
+    pass
