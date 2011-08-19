59b4cb00fefffbd0ed41a4a48fb7c2ef9c2c9f3a
Ijon Tichy <gztichy@lavabit.com>
2011-08-18 20:02:31 -0500

    + Added Y23H + Added QuadSHot * Downshifted ACS script numbers in jkemod.c by 26 * Finally remembered to add JSuperShotgun to Super Shotgun replacement * Hopefully fixed the Ionstrike not being on the ground online - Removed SuperShotNuke from BFG10k replacement

--- pk3/acs/jkemod.c
+++ pk3/acs/jkemod.c
@@ -6,12 +6,12 @@ int multigunStrings[3] = {
     "Ice shards",
     "Flame grenades"};
 
-script 900 (int trigger)  // (un)freeze the player
+script 874 (int trigger)  // (un)freeze the player
 {
     SetPlayerProperty(0, trigger, 0);
 }
 
-script 901 (void)
+script 875 (void)
 {
     int curMode = CheckInventory("MultigunMode");
     int newMode = (curMode + 1) % 3;
@@ -21,7 +21,7 @@ script 901 (void)
     Print(s:multigunStrings[newMode]);
 }
 
-script 902 (int speedInt, int speedFrac)
+script 876 (int speedInt, int speedFrac)
 {
     int speed = (speedInt << 16) + ((speedFrac << 16) / 100);
     SetActorProperty(0, APROP_Speed, speed);
