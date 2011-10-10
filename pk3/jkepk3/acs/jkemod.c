b8fea9fb0cf31977eb035156b42716844a13e095
Ijon Tichy <gztichy@lavabit.com>
2011-10-10 16:38:19 -0500

    * Editing FragmentLauncher - for some reason, git thing I'm re-adding everything

---
+++ pk3/jkepk3/acs/jkemod.c
@@ -0,0 +1,89 @@
+#library "jkemod"
+#include "zcommon.acs"
+
+#include "commonFuncs.h"
+
+int multigunStrings[3] = {
+    "Missiles",
+    "Ice shards",
+    "Flame grenades"};
+
+script 873 (int which, int arg1, int arg2)  // get inventory
+{
+    switch (which)
+    {
+        case 0:
+            if (arg1 == 0)
+            {
+                arg1 = 1;
+            }
+            int charge = CheckInventory("TristaffCharge");
+            SetResultValue(charge * arg1);
+            break;
+
+        case 1:
+            int ret = CheckInventory("JKEMana");
+            ret /= arg1;
+
+            SetResultValue(middle(1, ret, GetAmmoCapacity("TriStaffCharge") ) );
+            break;
+    }
+}
+
+script 874 (int trigger)  // (un)freeze the player
+{
+    SetPlayerProperty(0, trigger, 0);
+}
+
+script 875 (void)
+{
+    int curMode = CheckInventory("MultigunMode");
+    int newMode = (curMode + 1) % 3;
+
+    TakeInventory("MultigunMode", 0x7FFFFFFF);
+    GiveInventory("MultigunMode", newMode);
+    Print(s:multigunStrings[newMode]);
+}
+
+script 876 (int argInt, int argFrac, int which)
+{
+    int arg = (argInt << 16) + ((argFrac << 16) / 100);
+
+    ACS_ExecuteAlways(877, 0, arg, which, 0);
+}
+
+script 877 (int arg, int which)
+{
+    switch (which)
+    {
+        case 0:
+            SetActorProperty(0, APROP_Speed, arg);
+            break;
+
+        case 1:
+            SetActorProperty(0, APROP_JumpZ, arg);
+            break;
+
+        case 2:
+            SetActorProperty(0, APROP_RenderStyle, STYLE_None);
+            SetActorProperty(0, APROP_NoTarget, 1);
+            SetPlayerProperty(0, 1, 1);
+            break;
+
+        case 3:
+            SetActorProperty(0, APROP_RenderStyle, STYLE_Normal);
+            SetActorProperty(0, APROP_NoTarget, 0);
+            SetPlayerProperty(0, 0, 1);
+            break;
+
+        case 4:
+            SetActorProperty(0, APROP_DamageFactor, arg);
+            break;
+
+        case 5:
+            arg >>= 16;
+            SetActorProperty(0, APROP_SpawnHealth, arg);
+            SetActorProperty(0, APROP_Health, arg);
+            break;
+    }
+}
---
+++ pk3/jkepk3/acs/loopstuff.c
@@ -0,0 +1,120 @@
+#include "zcommon.acs"
+#library "loopstuff"
+
+
+int dead[32];
+int inGame[32];
+
+script 759 OPEN
+{
+    if (GetCVar("compat_clientssendfullbuttoninfo") == 0)
+    {
+        ConsoleCommand("compat_clientssendfullbuttoninfo 1");
+        Log(s:"compat_clientssendfullbuttoninfo must be on for the mod to work properly");
+    }
+}
+
+script 760 (void)   /* generic button signaler */
+{
+    int pln = PlayerNumber();
+    int buttons;
+    while (!dead[pln] && inGame[pln])
+    {
+        buttons = GetPlayerInput(-1, MODINPUT_BUTTONS);
+        if (buttons & BT_ATTACK)
+        {
+            GiveInventory("MainFired", 1);
+        }
+        else
+        {
+            TakeInventory("MainFired", 16);
+        }
+
+        if (buttons & BT_ALTATTACK)
+        {
+            GiveInventory("AltFired", 1);
+        }
+        else
+        {
+            TakeInventory("AltFired", 16);
+        }
+
+        Delay(1);
+    }
+}
+
+
+script 761 ENTER
+{
+    TakeInventory("ReloadButton", 1);
+    TakeInventory("PlayerDead", 1); // y'know, assuming you have it
+    inGame[PlayerNumber()] = 1;
+    dead[PlayerNumber()] = 0;
+    ACS_ExecuteAlways(760, 0, 0,0,0);
+    ACS_ExecuteAlways(730, 0, 0,0,0);
+
+    // best to assume we don't have it than assume we do
+    GiveInventory("ReloadButtonOn", 1);
+    GiveInventory("ReloadButtonOff", 1);
+}
+
+script 762 DEATH
+{
+    GiveInventory("PlayerDead", 1);
+    TakeInventory("ReloadButton", 1);
+    inGame[PlayerNumber()] = 1;
+    dead[PlayerNumber()] = 1;
+}
+
+script 763 (int pln) DISCONNECT
+{
+    dead[PlayerNumber()] = 0;
+    inGame[PlayerNumber()] = 0;
+}
+
+script 764 RESPAWN
+{
+    ACS_ExecuteAlways(761, 0, 0,0,0);
+}
+
+
+script 730 (void)
+{
+    int pln = PlayerNumber();
+    int check;
+    int powered;
+
+    while (!dead[pln] && inGame[pln])
+    {
+        if (!CheckInventory("IAmAMage") )
+        {
+            terminate;
+        }
+        Delay(1);
+        check += 1;
+
+        powered = CheckInventory("PowerWeaponLevel2");
+
+        if (check % 10 == 0)
+        {
+            GiveInventory("HellFistCounter", 1);
+        }
+
+        if ((check % 10) == 0 && powered)
+        {
+            GiveInventory("JKEMana", 1);
+        }
+
+
+        if (check % 20 == 0 && !powered)
+        {
+            GiveInventory("JKEMana", 1);
+        }
+
+        if (check % 20 == 0)
+        {
+            check = 0;
+        }
+
+    }
+}
